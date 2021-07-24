bb3280ce44b4170fb3a7f0917dd44f3aa8307318
Jadson Santos <42282908+jadson179@users.noreply.github.com>
2021-07-24 14:49:10 -0300

    Update __init__.py

--- custom_components/shell/__init__.py
+++ custom_components/shell/__init__.py
@@ -13,7 +13,18 @@ def setup(hass:HomeAssistant, config):
     def command(call: ServiceCall):
         command = call.data.get("command",None)
 
-        os.system(command)
+        if command:
+            os.system(command)
+
+    def commands(call: ServiceCall):
+        commands = call.data.get("commands",[str])
+
+        if commands:
+            for command in  commands:
+                os.system(command)
+
 
     hass.services.register(DOMAIN, "command", command)
+    hass.services.register(DOMAIN, "commands", commands)
+    
     return True
